a53c209a4ba7e8b048b8321e9551a3b3bb390afb
YilmazBatal <b.ogulbey@gmail.com>
2024-04-23 21:13:32 +0300

    Commented out

--- lib/Screens/product/order_details.dart
+++ lib/Screens/product/order_details.dart
@@ -1,19 +1,42 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
 
-class OrderDetailsPage extends StatelessWidget {
-  const OrderDetailsPage({super.key});
+// class OrderDetailsPage extends StatelessWidget {
+//   // const OrderDetailsPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Order Detail"),
-      ),
-      body: Column(
-        children: [
-          Text("data"),
-        ],
-      ),
-    );
-  }
-}
+//     final String image;
+//     final String title;
+
+//     const OrderDetailsPage({
+//       super.key,
+//       required this.image,
+//       required this.title,
+//     });
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Parametreleri almak için RouteData kullanın
+//     final params = GoRoute.of(context).queryParameters;
+    
+//     // param1 ve param2 değerlerini alın
+//     final param1 = params['param1'];
+//     final param2 = params['param2'];
+
+//     // İşlemlerinize devam edin...
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Details Page'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text('Parameter 1: $param1'),
+//             Text('Parameter 2: $param2'),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
