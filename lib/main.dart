3309bd204ff5f41e71ca05b87ee75d49e03d1376
YilmazBatal <b.ogulbey@gmail.com>
2024-04-24 16:05:23 +0300

    Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -6,13 +7,26 @@ import 'package:midterm_project/Screens/core/routes.dart';
 import 'package:midterm_project/bloc/client/client_cubit.dart';
 import 'package:midterm_project/bloc/client/client_state.dart';
 import 'package:midterm_project/Screens/core/localizations.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
+
+
+void main() async {
+  // Notification
+  WidgetsFlutterBinding.ensureInitialized();
+      await Permission.notification.isDenied.then((value) {
+        if (value) {
+          Permission.notification.request();
+        }
+      });
+      await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
+
+  
 
   @override
   Widget build(BuildContext context) {
